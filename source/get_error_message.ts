6cd8a8faf7e5441f3e942c26eddbff54bac2e41c
galactrax <anthonycweathersby@gmail.com>
2020-03-12 20:13:08 -0600

    Modify get_error_message.ts

--- source/get_error_message.ts
+++ source/get_error_message.ts
@@ -30,21 +30,23 @@ const EqualityLRMap = {
     bg = xtF(xtBold, xtColor(color.gray23)),
     bg2 = xtF(xtColor(color.lightpink3)),
     resB = xtF(xtReset),
-    res = xtF(xtReset);
+    rst = xtF(xtReset);
 
 export function getErrorMessageFromContent(node: MinTreeNode) {
     switch (node.type) {
+        case $.CallExpression:
+            return `Expected error to be thrown from ${fail + $r(node.nodes[0]) + rst}`;
         case $.EqualityExpression:
         case $.RelationalExpression:
             const left = ext(node).left, right = ext(node).right,
 
                 left_value = (left.type & MinTreeNodeClass.VARIABLE || left.type & MinTreeNodeClass.EXPRESSION)
-                    ? `${bg}[${ref + $r(left) + res} ⇒ ${val}\${s(${$r(left)})}${bg}]${resB + bg2}`
-                    : `${bg}[${ref}\${s(${$r(left)})}${bg}]${resB + bg2}`,
+                    ? `${bg}[${ref + $r(left) + rst} ⇒ ${val}\${i.makeLiteral(${$r(left)})}${bg}]${resB + bg2}`
+                    : `${bg}[${ref}\${i.makeLiteral(${$r(left)})}${bg}]${resB + bg2}`,
 
                 right_value = (right.type & MinTreeNodeClass.VARIABLE || right.type & MinTreeNodeClass.EXPRESSION)
-                    ? `${bg}[${fail + $r(right) + res} ⇒ ${val}\${s(${$r(right)})}${bg}]${resB + bg2}`
-                    : `${bg}[${fail}\${s(${$r(right)})}${bg}]${resB + bg2}`;
+                    ? `${bg}[${fail + $r(right) + rst} ⇒ ${val}\${i.makeLiteral(${$r(right)})}${bg}]${resB + bg2}`
+                    : `${bg}[${fail}\${i.makeLiteral(${$r(right)})}${bg}]${resB + bg2}`;
 
             if (right.type & MinTreeNodeClass.VARIABLE && !(left.type & MinTreeNodeClass.VARIABLE)) {
                 return `Expected ${right_value} ${EqualityRLMap[node.symbol]} ${left_value}`;
@@ -66,23 +68,39 @@ export function markExceptionExpression(message: string, pos: Lexer): string {
     if (pos.off <= 0) pos.off = start;
     else {
         pos.off++;
-        while (copy.ch !== ")") copy.next();
     }
 
-    const
-        match_string = copy.slice(pos),
-        ast = ext(exp(match_string), true);
+    let paren_count = 0;
+    while (!copy.END) {
+        if (copy.ch == "(")
+            paren_count++;
+        if (copy.ch == ")")
+            paren_count--;
+        if (paren_count < 0) break;
+        copy.next();
+    }
 
-    if (ast.type & MinTreeNodeClass.BINARY_EXPRESSION) {
+    let ast = null;
+
+    const match_string = copy.slice(pos);
+
+    try {
+        ast = ext(exp(match_string), true);
+    } catch (e) {
+        //  console.log(e);
+    }
+
+    if (ast && ast.type & MinTreeNodeClass.BINARY_EXPRESSION) {
+
         const { left, right, symbol } = ast,
             replace_string = [
                 ref + $r(left),
-                res + symbol,
-                fail + $r(right) + res + bg2
+                rst + symbol,
+                fail + $r(right) + rst + bg2
             ].join(" ");
 
         message = message.replace(match_string, replace_string);
     }
 
-    return bg2 + message + res;
+    return bg2 + message + rst;
 }
