8d24630ce0fdb6c47d866f8aca6fb5fd868a747a
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-03-16 08:34:47 -0600

    Update tests

--- test/load_test.spec.js
+++ test/load_test.spec.js
@@ -19,10 +19,10 @@ const LOADED = loadBindingCompiler(test_compiler);
 "loadBindingCompiler returns true if a binding compiler has been loaded into the manager";
 ((LOADED == true));
 
-"Returns false if an attempt is made to load an incomplete binding compiler";
+"Returns false if an attempt is made to load an incompatable binding compiler";
 ((loadBindingCompiler({ signature: MinTreeNodeType.AwaitExpression }) == false));
 
-"Returns an array of compilers whose signature matches the nodes type.";
+"Returns an array of compilers whose signature matches the nodes type";
 ((selectBindingCompiler(exp("a = 2")) == [test_compiler]));
 
 "Returns true if a binding compiler has been loaded into the manager 2";
@@ -31,5 +31,10 @@ const LOADED = loadBindingCompiler(test_compiler);
 "Expect to throw";
 ((!test()));
 
-"#AFTER EACH";
-clearBindingsCompilers();
+
+
+test_compiler.build();
+
+function AFTER_EACH(){
+    clearBindingsCompilers();
+}
---
+++ test/loop.spec.js
@@ -0,0 +1,25 @@
+"Loops";
+
+let i = 0;
+while (i < 200) {
+    "Handles Loops";
+    ((i >= 1))
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    "Handles Loops 2";
+    ((i < 200))
+}
+
+const g = [0, 1, 2, 3];
+
+for (var i = 0; i < 4;) {
+    ((i == g[i]))
+    i++
+}
+
