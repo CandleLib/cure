9557fa6aaf8f9b899a49128984017e616c2b9212
galactrax <anthonycweathersby@gmail.com>
2020-03-19 00:09:45 -0600

    Remove default parameter values

--- source/reporting/report.ts
+++ source/reporting/report.ts
@@ -3,14 +3,14 @@ import { BasicReporter } from "./basic_reporter.js";
 import { Reporter } from "../types/reporter.js";
 
 const c = new CLITextDraw();
-export async function startRun(pending_tests, suites, reporter: Reporter = new BasicReporter(), console: CLITextDraw | Console = c) {
-    return reporter.start(pending_tests, suites, console);
+export async function startRun(pending_tests, suites, reporter: Reporter, term: CLITextDraw | Console = c) {
+    return reporter.start(pending_tests, suites, term);
 }
 
-export async function updateRun(completed_tests, suites, reporter: Reporter = new BasicReporter(), console: CLITextDraw | Console = c) {
-    return reporter.update(completed_tests, suites, console);
+export async function updateRun(completed_tests, suites, reporter: Reporter, term: CLITextDraw | Console = c) {
+    return reporter.update(completed_tests, suites, term);
 }
 
-export async function completedRun(completed_tests, suites, reporter: Reporter = new BasicReporter(), console: CLITextDraw | Console = c) {
-    return reporter.complete(completed_tests, suites, console);
+export async function completedRun(completed_tests, suites, reporter: Reporter, term: CLITextDraw | Console = c) {
+    return reporter.complete(completed_tests, suites, term);
 }
