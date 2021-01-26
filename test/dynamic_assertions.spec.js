c61ed56b2e65b25654dee712f5200c6528bde6e0
Anthony Weathersby <anthonycweathersby@gmail.com>
2021-01-26 15:38:52 -0700

    Test: Modify dynamic test

--- test/dynamic_assertions.spec.js
+++ test/dynamic_assertions.spec.js
@@ -1,4 +1,5 @@
 import URL from "@candlefw/url";
+import { assert } from "console";
 import { createTestRigsFromStringSource, createTestSuiteFromSource, getSuiteTestOutcomeFromSource } from "./tools.js";
 
 await URL.server();
@@ -13,12 +14,23 @@ assert("No import names", raw_rigs[0].import_names.size == 0);
 
 //Run the test
 const suite = await createTestSuiteFromSource(source);
-assert(suite != null);
+
+assert(suite == null);
 assert(suite.error == null);
-assert(suite.rigs.length == 1);
+assert(suite.rigs.length == 8);
 
 const outcome = await getSuiteTestOutcomeFromSource(source);
 
 assert(outcome != null);
 assert(outcome.FAILED == true);
-assert(outcome.results.length == 10);
+assert("32 Test Results have been dynamically generated", outcome.results.length == 32);
+assert(solo, "32 Test Names have been dynamically generated", outcome.results.map(r => r.name) == [
+    "While A0", "While A1", "While A2", "While A3",
+    "While B0", "While B1", "While B2", "While B3",
+    "For(;;) A0", "For(;;) A1", "For(;;) A2", "For(;;) A3",
+    "For(;;) B0", "For(;;) B1", "For(;;) B2", "For(;;) B3",
+    "For of A0", "For of A1", "For of A2", "For of A3",
+    "For of B0", "For of B1", "For of B2", "For of B3",
+    "Do While A0", "Do While A1", "Do While A2", "Do While A3",
+    "Do While B0", "Do While B1", "Do While B2", "Do While B3",
+]);
