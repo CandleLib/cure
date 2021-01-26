5c6ac4cf70af602da7a8ae452de215c799d4cc4b
Anthony Weathersby <anthonycweathersby@gmail.com>
2021-01-26 10:53:54 -0700

    Test: Add tests for dynamic generated test results

--- test/dynamic_assertions.spec.js
+++ test/dynamic_assertions.spec.js
@@ -1,10 +1,24 @@
 import URL from "@candlefw/url";
-import { createTestRigsFromStringSource } from "./tools.js";
+import { createTestRigsFromStringSource, createTestSuiteFromSource, getSuiteTestOutcomeFromSource } from "./tools.js";
 
 await URL.server();
 
-const rigs = createTestRigsFromStringSource(await (URL.resolveRelative("./data/dynamic_test.js")).fetchText());
+const source = await (URL.resolveRelative("./data/dynamic_test.js")).fetchText();
 
-assert("Rigs object is not undefined", rigs !== undefined);
-assert("One RawTestRig object created", rigs.length == 1);
-assert("No Import Names", rigs[0].import_names.size == 0);
+const raw_rigs = createTestRigsFromStringSource(source);
+
+assert("Rigs object is not undefined", raw_rigs !== undefined);
+assert("One RawTestRig object created", raw_rigs.length == 1);
+assert("No import names", raw_rigs[0].import_names.size == 0);
+
+//Run the test
+const suite = await createTestSuiteFromSource(source);
+assert(suite != null);
+assert(suite.error == null);
+assert(suite.rigs.length == 1);
+
+const outcome = await getSuiteTestOutcomeFromSource(source);
+
+assert(outcome != null);
+assert(outcome.FAILED == true);
+assert(outcome.results.length == 10);
--- test/tools.js
+++ test/tools.js
@@ -1,16 +1,55 @@
 import URL from "@candlefw/url";
-import { compileRawTestRigs } from "@candlefw/test/build/library/compile/compile_statements.js";
-import { InitializeReporterColors } from "@candlefw/test/build/library/utilities/create_test_frame.js";
 import { parser } from "@candlefw/js";
-import { BasicReporter } from "@candlefw/test";
-const reporter = new BasicReporter;
-InitializeReporterColors(reporter);
+import { NullReporter } from "@candlefw/test";
+import { compileRawTestRigs } from "../build/library/compile/compile_statements.js";
+import { createGlobals, createTestSuite, initializeReporterColors } from "../build/library/utilities/create_test_frame.js";
+import { loadTests } from "../build/library/utilities/load_tests.js";
+import { runTests } from "../build/library/test_running/run_tests.js";
+import { RunnerBoss } from "../build/library/test_running/runner_boss.js";
 
 await URL.server();
 
 export function createTestRigsFromStringSource(source) {
 
+    const reporter = new NullReporter;
+
+    initializeReporterColors(reporter);
+
     const { raw_rigs: rigs } = compileRawTestRigs(parser(source).ast, reporter, []);
 
     return rigs;
+}
+
+function createGlobalsObject(report_constructor = NullReporter) {
+    const globals = createGlobals(1000, "internal", false, false, false);
+
+    globals.reporter = initializeReporterColors(new report_constructor());
+
+    return globals;
+}
+
+export async function createTestSuiteFromSource(source, globals = createGlobalsObject()) {
+
+    const suite = createTestSuite("internal", 0);
+
+    await loadTests(source, suite, globals);
+
+    suite.name = "test";
+
+    return suite;
+}
+
+export async function getSuiteTestOutcomeFromSource(source) {
+
+    const globals = createGlobalsObject();
+
+    globals.runner = new RunnerBoss(1);
+
+    globals.suites = new Map([["dd", await createTestSuiteFromSource(source, globals)]]);
+
+    const suites = Array.from(globals.suites.values());
+
+    await runTests(suites.flatMap(suite => suite.rigs), Array.from(suites), globals);
+
+    return globals.outcome;
 }
