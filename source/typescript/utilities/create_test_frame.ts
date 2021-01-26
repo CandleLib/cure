ea4d5988bfa646b8f055e5b8d66eb6d8a2b0b833
Anthony Weathersby <anthonycweathersby@gmail.com>
2021-01-26 10:32:20 -0700

    Refactor: Refactor create_test_frame

--- source/typescript/utilities/create_test_frame.ts
+++ source/typescript/utilities/create_test_frame.ts
@@ -11,6 +11,235 @@ import * as colors from "./colors.js";
 import { TestFrameOptions } from "../types/test_frame_options";
 import { TestError } from "../test_running/test_error.js";
 import { getPackageJsonObject } from "@candlefw/wax";
+import { TestSuite } from "../types/test_suite.js";
+
+
+const DefaultOptions: TestFrameOptions = {
+    test_dir: "",
+    PRELOAD_IMPORTS: false,
+    WATCH: false,
+    number_of_workers: 1,
+    assertion_compilers: [],
+    max_timeout: 2000
+    //test_globs: [path.resolve(process.cwd(), "test/**/*")],
+};
+
+type Resolver = (value: Outcome | PromiseLike<Outcome>) => void;
+
+/**
+ * Loads tests files and returns a TestFrame from which tests can be run. 
+ * 
+ * @param {TestFrameOptions} - A TestFrameOptions object.
+ * @param {string[]} test_suite_url_strings - An array of file paths to retrieve test files from.
+ */
+export function createTestFrame(
+    config_options: TestFrameOptions,
+    ...test_suite_url_strings: string[]
+): TestFrame {
+
+    const {
+        PRELOAD_IMPORTS = false,
+        WATCH = false,
+        number_of_workers = 2,
+        test_dir,
+        max_timeout,
+        BROWSER_HEADLESS
+    } = Object.assign(<TestFrameOptions>{}, DefaultOptions, config_options);
+
+
+    let
+        resolution: Resolver = null,
+
+        globals = createGlobals(
+            max_timeout,
+            test_dir,
+            WATCH,
+            PRELOAD_IMPORTS,
+            BROWSER_HEADLESS,
+            resolution
+        );
+
+    function initializeResolver(res: Resolver) {
+
+        if (resolution) endWatchedTests(globals, resolution);
+
+        resolution = res;
+    }
+
+    return {
+
+        setReporter: (reporter: Reporter) => {
+            globals.reporter = initializeReporterColors(reporter);
+        },
+
+        get number_of_workers() { return number_of_workers; },
+
+        get WATCHED() { return WATCH; },
+
+        endWatchedTests: () => endWatchedTests(globals, resolution),
+
+        start: (): Promise<Outcome> => new Promise(async (resolver: Resolver) => {
+
+            await URL.server();
+
+            initializeResolver(resolver);
+
+            await initializeGlobals(globals, number_of_workers);
+
+            await loadAndRunTestSuites(globals, test_suite_url_strings);
+
+            watchTestsOrExit(globals, resolution);
+        })
+    };
+};
+
+
+
+async function loadAndRunTestSuites(globals: Globals, test_suite_url_strings: string[]) {
+
+    globals.flags.PENDING = true;
+
+    try {
+
+        const st = await loadTestSuites(test_suite_url_strings, globals);
+
+        await runTests(st.flatMap(suite => suite.rigs), st, globals);
+
+    } catch (e) {
+        globals.outcome.errors.push(new TestError(e, "", 0, 0, "", "", undefined));
+    }
+
+    globals.flags.PENDING = false;
+}
+
+async function initializeGlobals(globals: Globals, number_of_workers: number) {
+
+    if (!globals.reporter) globals.reporter = initializeReporterColors(new BasicReporter());
+
+    await loadPackageJson(globals);
+
+    globals.runner = new RunnerBoss(Math.max(number_of_workers, 1));
+
+    globals.watchers.length = 0;
+
+    if (globals.flags.USE_HEADLESS_BROWSER) globals.reporter.notify("-- browser tests will be run in a headless browser --");
+
+}
+
+async function loadPackageJson(globals: Globals) {
+    const { package: pkg, FOUND: PACKAGE_FOUND, package_dir } = await getPackageJsonObject(process.cwd() + "/");
+
+    if (PACKAGE_FOUND) {
+        globals.package_name = pkg?.name ?? "";
+        globals.package_dir = new URL(package_dir);
+        globals.package_main = pkg?.main ?? "";
+    }
+}
+
+async function loadTestSuites(test_suite_url_strings: string[], globals: Globals) {
+
+    globals.suites = new Map(test_suite_url_strings.map((url_string, index) => [
+        url_string, createTestSuite(url_string, index)
+    ]));
+
+    for (const suite of globals.suites.values())
+        await loadSuite(suite, globals);
+
+    const st = Array.from(globals.suites.values());
+
+    return st;
+}
+
+function watchTestsOrExit(globals: Globals, resolution: any) {
+
+    if (globals.flags.WATCH) {
+
+        globals.reporter.notify("Waiting for changes...");
+
+        process.on("exit", () => {
+
+            globals.reporter.notify("EXITING");
+
+            endWatchedTests(globals, resolution);
+        });
+
+    } else {
+
+        endWatchedTests(globals, resolution);
+    }
+}
+
+export function createGlobals(
+    max_timeout: number = 1000,
+    test_dir: string = "",
+    WATCH: boolean = false,
+    PRELOAD_IMPORTS: boolean = false,
+    BROWSER_HEADLESS: boolean = false,
+    resolution: any = null
+): Globals {
+    const globals: Globals = {
+        default_retries: 1,
+
+        max_timeout,
+
+        flags: {
+            PRELOAD_IMPORTS,
+
+            PENDING: false,
+
+            WATCH,
+
+            USE_HEADLESS_BROWSER: BROWSER_HEADLESS
+        },
+
+        test_dir,
+
+        package_name: "",
+
+        package_dir: new URL,
+
+        package_main: "",
+
+        suites: null,
+
+        reporter: null,
+
+        runner: null,
+
+        watchers: [],
+
+        watched_files_map: new Map(),
+
+        outcome: { FAILED: true, results: [], errors: [] },
+
+        exit: (reason = "Exiting for an unknown reason", error) => {
+
+            const { fail } = globals.reporter.colors;
+
+            globals.reporter.notify("\n" + fail + reason + colors.rst + "\n");
+
+            if (error) {
+                //Make sure this is printed no matter what.
+                console.error(error);
+                globals.outcome.errors.push(new TestError(error));
+            }
+
+            endWatchedTests(globals, resolution);
+        }
+    };
+
+    return globals;
+}
+
+export function createTestSuite(url_string: string, index: number): TestSuite {
+    return {
+        origin: url_string,
+        rigs: [],
+        index,
+        data: "",
+        name: ""
+    };
+}
 
 function endWatchedTests(globals: Globals, resolution: (arg: Outcome) => void) {
 
@@ -43,177 +272,7 @@ function endWatchedTests(globals: Globals, resolution: (arg: Outcome) => void) {
     }
 }
 
-export function InitializeReporterColors(reporter: Reporter): Reporter {
+export function initializeReporterColors(reporter: Reporter): Reporter {
     reporter.colors = Object.assign({}, colors, reporter.colors);
     return reporter;
 }
-
-const DefaultOptions: TestFrameOptions = {
-    test_dir: "",
-    PRELOAD_IMPORTS: false,
-    WATCH: false,
-    number_of_workers: 1,
-    assertion_compilers: [],
-    max_timeout: 2000
-    //test_globs: [path.resolve(process.cwd(), "test/**/*")],
-};
-
-/**
- * Loads tests files and returns a TestFrame from which tests can be run. 
- * 
- * @param {TestFrameOptions} - A TestFrameOptions object.
- * @param {string[]} test_suite_url_strings - An array of file paths to retrieve test files from.
- */
-export function createTestFrame(
-    config_options: TestFrameOptions,
-    ...test_suite_url_strings: string[]
-): TestFrame {
-
-    const {
-        PRELOAD_IMPORTS = false,
-        WATCH = false,
-        number_of_workers = 2,
-        assertion_compilers = [],
-        test_dir,
-        max_timeout,
-        BROWSER_HEADLESS
-    } = <TestFrameOptions>Object.assign({}, DefaultOptions, config_options);
-
-    let
-        resolution = null,
-
-        globals: Globals = {
-
-            default_retries: 1,
-
-            max_timeout,
-
-            flags: {
-
-                PRELOAD_IMPORTS,
-
-                PENDING: false,
-
-                WATCH,
-
-                USE_HEADLESS_BROWSER: BROWSER_HEADLESS
-            },
-
-            test_dir,
-
-            package_name: "",
-
-            package_dir: new URL,
-
-            package_main: "",
-
-            suites: null,
-
-            reporter: null,
-
-            runner: null,
-
-            watchers: [],
-
-            watched_files_map: new Map(),
-
-            outcome: { FAILED: true, results: [], errors: [] },
-
-            exit: (reason = "Exiting for an unknown reason", error) => {
-
-                const { fail } = globals.reporter.colors;
-
-                globals.reporter.notify("\n" + fail + reason + colors.rst + "\n");
-
-                if (error) {
-                    //Make sure this is printed no matter what.
-                    console.error(error);
-                    globals.outcome.errors.push(new TestError(error));
-                }
-
-                endWatchedTests(globals, resolution);
-            }
-        };
-
-    return {
-
-        setReporter: (reporter: Reporter) => {
-            globals.reporter = InitializeReporterColors(reporter);
-        },
-
-        get number_of_workers() { return number_of_workers; },
-
-        get WATCHED() { return WATCH; },
-
-        endWatchedTests: () => endWatchedTests(globals, resolution),
-
-        start: (): Promise<Outcome> => new Promise(async (res) => {
-
-            if (!globals.reporter)
-                globals.reporter = InitializeReporterColors(new BasicReporter());
-
-            await URL.server();
-
-            const { package: pkg, FOUND: PACKAGE_FOUND, package_dir }
-                = await getPackageJsonObject(process.cwd() + "/");
-
-            if (PACKAGE_FOUND) {
-                globals.package_name = pkg?.name ?? "";
-                globals.package_dir = new URL(package_dir);
-                globals.package_main = pkg?.main ?? "";
-            }
-
-            if (resolution)
-                endWatchedTests(globals, resolution);
-
-            resolution = res;
-
-            globals.suites = new Map(test_suite_url_strings.map((url_string, index) => [
-                url_string, { origin: url_string, rigs: [], index, data: "", name: "" }
-            ]));
-
-            globals.runner = new RunnerBoss(Math.max(number_of_workers, 1));
-
-            globals.watchers.length = 0;
-
-            const { suites } = globals;
-
-            if (globals.flags.USE_HEADLESS_BROWSER)
-                globals.reporter.notify("-- browser tests will be run in a headless browser --");
-
-            try {
-
-                for (const suite of suites.values())
-                    await loadSuite(suite, globals);
-
-                const st = Array.from(suites.values());
-
-                globals.flags.PENDING = true;
-
-                await runTests(st.flatMap(suite => suite.rigs), st, globals);
-
-            } catch (e) {
-                globals.outcome.errors.push(new TestError(e, "", 0, 0, "", "", undefined));
-            }
-
-            globals.flags.PENDING = false;
-
-            if (WATCH) {
-
-                globals.reporter.notify("Waiting for changes...");
-
-                process.on("exit", () => {
-
-                    globals.reporter.notify("EXITING");
-
-                    endWatchedTests(globals, resolution);
-                });
-
-            } else {
-
-                endWatchedTests(globals, resolution);
-            }
-        })
-    };
-}
-;
