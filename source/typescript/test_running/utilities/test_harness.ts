2b8f699f7b1e80bbaf217df8359b61e0f531bf26
Anthony Weathersby <anthonycweathersby@gmail.com>
2021-01-31 23:39:34 -0700

    Fix: Ensure objects are converted to string in makeLiteral method

--- source/typescript/test_running/utilities/test_harness.ts
+++ source/typescript/test_running/utilities/test_harness.ts
@@ -135,7 +135,7 @@ export function createTestHarnessEnvironmentInstance(equal, util, performance: P
 
                     default:
 
-                        return value;
+                        return value.toString();
                 }
             },
 
